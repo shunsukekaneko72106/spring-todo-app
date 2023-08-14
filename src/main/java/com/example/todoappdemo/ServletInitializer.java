9faa88783aed23ae2cca53dc51f4ae63e7f251a6
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2023-08-14 19:53:25 +0900

    first commit

---
+++ src/main/java/com/example/todoappdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.todoappdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodoAppDemoApplication.class);
+	}
+
+}
---
+++ src/test/java/com/example/todoappdemo/TodoAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
